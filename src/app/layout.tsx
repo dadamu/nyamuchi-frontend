efa4c19c246f8d4c1896ea4c7439eb0a71933ed3
VirtualDruid <virtualdruid70@gmail.com>
2024-11-26 10:34:56 +0800

    google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 //import { Inter } from "next/font/google";
 //import "./globals.css";
 
-import { Analytics } from '@vercel/analytics/react';
+//import { Analytics } from '@vercel/analytics/react';
 import ReactGA from 'react-ga4';
 //const inter = Inter({ subsets: ["latin"] });
 ReactGA.initialize("G-RZRPMDK12F");
@@ -14,11 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" style={{backgroundColor:"#3381AF", height:"100%"}}>
+    <html lang="en" style={{ backgroundColor: "#3381AF", height: "100%" }}>
+      <head>
       <title>MYGO</title>
-      <body>{children}
-        <Analytics/>
-      </body>
+        <meta name="google-site-verification" content="_lnG5v_S320tjhKPWVgfjRrB8yBWmyxZRBrJmbgA2Hs" />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
